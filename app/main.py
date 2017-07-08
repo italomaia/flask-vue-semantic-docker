6af3d3874cedfd680996504faecba382b49f6149
Italo Maia <italo.maia@gmail.com>
2017-07-08 11:53:43 -0300

    adding test view for app

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from empty import Empty
 
 
 class App(Empty):
+    def configure_views(self):
+        @self.route('/hello')
+        def index():
+            """Use this to make sure your web app is reachable"""
+            return 'It Works'
+
     def configure_error_handlers(self):
         """SPA"""
         pass
