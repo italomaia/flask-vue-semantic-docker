10ee289ace71c53a07ac58f43fd34fdf3570cd33
Italo Maia <italo.maia@gmail.com>
2017-07-10 21:43:35 -0300

    Added configuration to load extensions and auth

--- app/config.py
+++ app/config.py
@@ -2,6 +2,21 @@ import os
 
 DEBUG = os.getenv('FLASK_DEBUG') == '1'
 
+BLUEPRINTS = ['auth']
 EXTENSIONS = list(map(lambda e: 'extensions.' + e, [
     'io',
+    'db',
+    'migrate',
+    'glue',
+    'ma',
+    'security'
 ]))
+
+PSYCOPG2_URI = 'postgresql+psycopg2://{user}:{passwd}@{host}/{name}'
+SQLALCHEMY_DATABASE_URI = PSYCOPG2_URI.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host='db',
+    name=os.getenv('POSTGRES_DB')
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
