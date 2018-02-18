2466ab110aca6c952ff8c652369509b9d1490a88
Italo Maia <italo.maia@gmail.com>
2018-02-18 18:20:38 -0300

    Ignore styles lint rule

--- fabfile.py
+++ fabfile.py
@@ -72,6 +72,10 @@ def update_webpack_base_conf(conf_path):
     line_condition = 'module.exports = {'
     insert_line_after(lines, line_to_insert, line_condition)
 
+    line_to_insert = "exclude: [resolve('src/styles')],\n"
+    line_condition = "include: [resolve('src'), resolve('test')]"
+    insert_line_after(lines, line_to_insert, line_condition)
+
     with open(conf_path, 'w') as fs:
         fs.write(''.join(lines))
 
